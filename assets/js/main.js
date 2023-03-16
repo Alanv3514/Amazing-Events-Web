f98d68cbd110a46004f492edab7fc1396d3ef4af
Alanv3514 <alanv3514@gmail.com>
2023-03-16 15:45:43 -0300

    index merge to vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,56 @@
-fetch(datajson)
-    .then(response => { return response.json() })
-    .then(data => {
-        for (let event of data.events) {
-            currentDate = new Date(data.currentDate);
-            categoriasHtml += saveCategoriesHtml(event.category);// llamo a save Categorieshtml (guarda en categoriasHtml un str con los elementos del dom que representan nuestra checkboxes list.)
-            htmlEvents += generateCards(event._id, event.image, event.name, event.description, event.price, event.category);// llamo a generateCards (guarda en htmlEvents un str con los elementos del dom que representan las tarjetas.)
-        };
+const { createApp } = Vue
 
-        searchInput = document.getElementById('search-input');//obtenemos el input de texto para el filtro buscador
-        searchButton = document.getElementById('search-button');//obtenemos el input de boton para el filtro 
-        cardContainer = document.getElementById('cardC');//obtenemos el container para las cards
-        categoriesContainer=  document.getElementById("chkCategories"); // obtenemos el container para nuestros checkboxes
-        msg = document.getElementById('voidMsg');//obtenemos el elemento del dom que corresponde a un mensaje predeterminado
-
-        cardContainer.innerHTML = htmlEvents;//inyectamos las tarjetas
-        categoriesContainer.innerHTML = categoriasHtml;//inyectamos las checkboxes
-
-        searchCategory = document.querySelectorAll("input[type='checkbox']")//obtenemos los elementos del dom que corresponden a los checkboxes
-
-        //declaramos los listeners
-        searchButton.addEventListener('click', filterCards);
-        searchInput.addEventListener('input', filterCards);
-
-        for (let checkbox of searchCategory) {
-            checkbox.addEventListener('change',filterCards)//asignamos un listener a cada checkbox
+const app = createApp({
+    data(){
+        return {
+            urlApi: 'https://mindhub-xj03.onrender.com/api/amazing',
+            dato: [],
+            eventos: [],
+            bkpEventos: [],
+            texto: '',
+            categorias: [],
+            selectCategorias: [],
         }
-        }
-    )
+    },
+    created(){
+        this.traerDatos()
+    },
+    mounted(){
 
+    },
+    methods:{
+        traerDatos(){
+            fetch(this.urlApi)
+                .then(response => response.json())
+                .then(data => {
+                    this.dato = data
+                    for (let event of data.events) {
+                        this.eventos.push(event);
+                        this.bkpEventos.push(event);
+                        this.setCat(this.eventos)
+                    };
+                })
+                .catch(error => console.log(error.message))
+        },
+        setCat(array){
+            array.forEach(event => {
+                if(!this.categorias.includes(event.category)){
+                    this.categorias.push(event.category)
+                }
+            })
+        },
+
+    },
+    computed:{
+        filtroDoble(){
+            let primerFiltro = this.bkpEventos.filter(evento => (evento.name.toLowerCase().includes(this.texto.toLowerCase())||evento.description.toLowerCase().includes(this.texto.toLowerCase())))
+            if(this.selectCategorias.length > 0){
+                this.eventos = primerFiltro.filter(evento => this.selectCategorias.includes(evento.category))
+            } else {
+                this.eventos = primerFiltro
+            }
+        }
+    },
+})
+
+app.mount('#app')
---
+++ assets/js/old/stats.js
@@ -0,0 +1,176 @@
+//Declaracion de variables a utilizar
+let pasCatStats = []; //Guardaremos informacion sobre las Categorias de eventos pasados
+let futCatStats = [];//Guardaremos informacion sobre las Categorias de eventos futuros
+let HAevent = []; //Guardaremos informacion sobre la mayor asistencia a eventos
+let LAevent = []; //Guardaremos informacion sobre la menor asistencia a eventos
+let htmlTable = ``; //String donde crearemos nuestra tabla antes de inyectarla al html
+let pastEvents =[];
+fetch(datajson)
+    .then(response => { return response.json() })
+    .then(data => {
+        for (let event of data.events) {
+            let eventDate = new Date(event.date);
+            currentDate = new Date(data.currentDate);
+            if (eventDate > currentDate) {//si es un evento Futuro
+                event.assistance = event.estimate; //agrego la propiedad assistance igual a estimate para evitar problemas futuros de calculo
+                let MontoRecaudado = event.price * event.estimate;//calculo el monto recaudado por el evento
+                statsByCat(futCatStats, event, MontoRecaudado, event.estimate, event.capacity);// Lleno futCatFiltrados
+                event.pattendance = (event.assistance / event.capacity * 100);//Agrego la propiedad pattendance (Porcentaje de asistencia) al evento
+            
+            }
+            else {
+                event.estimate = event.assistance;//agrego la propiedad estimate igual a assistance para evitar problemas futuros de calculo
+                let MontoRecaudado = event.price * event.assistance;//calculo el monto recaudado por el evento
+                statsByCat(pasCatStats, event, MontoRecaudado, event.assistance, event.capacity);//LLeno pasCatFiltrados
+                event.pattendance = (event.assistance / event.capacity * 100);//Agrego la propiedad pattendance (Porcentaje de asistencia) al evento
+                pastEvents.push(event);
+            }
+            
+        }
+        assistancesLogic(HAevent, LAevent, pastEvents);//LLeno tanto LAevent como HAevent. 
+        htmlTable= makeTable(HAevent, LAevent, pastEvents);//Creo el String con la tabla
+        let tablita = document.getElementById("table-Stats");//capturo el elemento que representa mi tabla
+        tablita.innerHTML = htmlTable;//inyecto el html en el dom
+
+    });
+
+/**
+ * Funcion que dandole los siguientes parametros nos rellena el array de eventos con las estadisticas de la categoria correspondiente al evento en cuestion.
+ * @param {*} event evento que queremos evaluar
+ * @param {*} eventArray array de eventos que queremos revisar y modificar
+ * @param {*} MontoRecaudado monto recaudado por el evento que queremos evaluar
+ * @param {*} Asistencias cantidad de asistencias al evento
+ * @param {*} Capacity capacidad del evento
+ */
+function statsByCat(eventArray,event, MontoRecaudado, Asistencias, Capacity) {
+    var index = eventArray.findIndex(function (obj) {
+        return obj.nameCat === event.category;
+    });
+
+    if (index == -1) {
+        eventArray.push({
+            nameCat: event.category,
+            recaudado: MontoRecaudado,
+            asistencias: Asistencias,
+            capacity: Capacity,
+        });
+    }
+    else {
+        eventArray[index].recaudado += MontoRecaudado;
+        eventArray[index].asistencias += Asistencias;
+        eventArray[index].capacity += Capacity;
+    }
+}
+
+
+
+/**
+ * Funcion que rellena HAevent y LAevent en base al array de eventos que le pasemos
+ * @param {*} HAevent array de eventos con mayor asistencias
+ * @param {*} LAevent array de eventos con menor asistencias
+ * @param {*} allEvents array con todos los eventos a evaluar
+ */
+function assistancesLogic(HAevent, LAevent, allEvents) {
+
+    allEvents.sort(function (a, b) { return b.pattendance - a.pattendance; })
+    for (i = 0; i < allEvents.length / 4; i++) {
+        HAevent.push(allEvents[i]);
+    }
+
+    allEvents.sort(function (a, b) { return a.pattendance - b.pattendance; })
+    for (i = 0; i < allEvents.length / 4; i++) {
+        LAevent.push(allEvents[i]);
+    }
+
+}
+
+
+/**
+ * Funcion que crea una tabla en funcion e los array de eventos que le pasemos
+ * @param {*} HAevent array de eventos con mayor asistencias
+ * @param {*} LAevent array de eventos con menor asistencias
+ * @param {*} allEvents array con todos los eventos a evaluar
+ * @returns retorna un String con la Tabla creada
+ */
+function makeTable(HAevent, LAevent, allEvents) {
+    let Table=``;
+    Table = `<div class="table-responsive pt-lg-5 pb-lg-5 pe-lg-5 ps-lg-2 me-lg-5 ms-lg-5">
+            <table class="table table-bordered border-dark ">
+                <thead>
+                    <tr>
+                        <th class="bg-body-secondary" colspan="3">Events Statistics</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr >
+                        <th >Events with the highest percentage of attendance</th>
+                        <th>Events with the lowest percentage of attendance</th>
+                        <th>Events with larger capacity</th>
+                    </tr>`
+          Table += HAStatsTable (HAevent, LAevent, allEvents);
+          Table += `<tr>
+                        <th class="bg-body-secondary" colspan="3">Upcoming events statistics by category</th>
+                    </tr>
+                    <tr >
+                        <th>Categories</th>
+                        <th>Revenues</th>
+                        <th>Percentage of attendance</th>
+                    </tr>`
+          Table += CatStatisticsTable(futCatStats);
+          Table += `<tr>
+                        <th class="bg-body-secondary" colspan="3">Past events statistics by category</th>
+                    </tr>
+                    <tr >
+                        <th>Categories</th>
+                        <th>Revenues</tg>
+                        <th>Percentage of attendance</th>
+                    </tr>`
+    Table += CatStatisticsTable(pasCatStats);
+    Table += `</tbody>
+            </table>
+        </div>`
+        return Table;
+}
+
+/**
+ * Funcion que crea las <tr></tr> de las estadisticas de categorias
+ * @param {*} CatFiltrados Array de stadisticas de categorias
+ * @returns retorna un String con el html correspondiente a su insercion en una tabla
+ */
+function CatStatisticsTable(CatStats){
+    let trHtml=``;
+    for (i = 0; ((i < CatStats.length)); i++) {
+        let percent = ((CatStats[i].asistencias) / (CatStats[i].capacity) * 100)
+        trHtml+= `<tr>
+                        <td>${CatStats[i].nameCat}</td>
+                        <td>$${CatStats[i].recaudado}</td>
+                        <td>${percent.toFixed(2)}%</td>
+                    </tr>`
+    }
+    return trHtml;
+}
+
+
+/**
+ * Funcion que crea las <tr></tr> de los Porcentajes de Asistencias
+ * @param {*} HAevent array de eventos con mayor asistencias
+ * @param {*} LAevent array de eventos con menor asistencias
+ * @param {*} allEvents array con todos los eventos a evaluar
+ * @returns retorna un String con el html correspondiente a su insercion en una tabla
+ */
+function HAStatsTable (HAevent, LAevent, allEvents){
+    let HAStatsHtml=``;
+    allEvents.sort(function (a, b) { return b.capacity - a.capacity; });
+    for (i = 0; i < Math.max(HAevent.length, LAevent.length); i++) {
+        const LAeventPercent = LAevent.hasOwnProperty(i) ? ` : ${(LAevent[i].assistance / LAevent[i].capacity * 100).toFixed(2)}%` : '';
+        const HAeventPercent = HAevent.hasOwnProperty(i) ? ` : ${(HAevent[i].assistance / HAevent[i].capacity * 100).toFixed(2)}%` : '';
+        const HAeventName = HAevent.hasOwnProperty(i) ? `${HAevent[i].name}${HAeventPercent} ` : '';
+        const LAeventName = LAevent.hasOwnProperty(i) ? `${LAevent[i].name}${LAeventPercent} ` : '';
+        HAStatsHtml += `<tr>
+                            <td>${HAeventName}</td>
+                            <td>${LAeventName}</td>
+                            <td>${allEvents[i].name} : ${allEvents[i].capacity} </td>
+                        </tr>`;
+    }
+    return HAStatsHtml;
+}
