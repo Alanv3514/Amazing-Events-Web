7df327a81136c73241388f27a8b10c7de18b924f
Alanv3514 <alanv3514@gmail.com>
2023-03-16 20:32:10 -0300

    merge details to vue

---
+++ assets/js/details.js
@@ -0,0 +1,41 @@
+const { createApp } = Vue
+
+
+
+const app = createApp({
+    data(){
+        return {
+            urlApi: 'https://mindhub-xj03.onrender.com/api/amazing',
+            dato: [],
+            evento:{},
+        }
+        
+    },
+    created(){
+        this.traerDatos()
+    },
+    mounted(){
+
+    },
+    methods:{
+        traerDatos(){
+            const queryString = location.search; //creamos una constante con el search
+            const params = new URLSearchParams(queryString); //creamos un objeto del tipo queryString
+            const id = params.get("id");//obtenemos el parametro ID que pasamos por el url    
+            fetch(this.urlApi)
+                .then(response => response.json())
+                .then(data => {
+                    this.dato = data
+                    this.evento = data.events.find(evento => evento._id ==id); //seleccionamos dentro de nuestro data el evento correspondiente al id que leimos en el url  
+                })
+                
+
+                .catch(error => console.log(error.message))
+        },
+
+    },
+    computed:{
+    },
+})
+
+app.mount('#app')
